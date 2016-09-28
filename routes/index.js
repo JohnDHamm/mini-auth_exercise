c5f803bce4323ff152ad0ecae05b2c3754a6e2a9
John Hamm <johndhamm1@gmail.com>
2016-09-27 21:49:10 -0500

    server configured

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const { Router } = require('express');
+// const bcrypt = require('bcrypt')
+
+const router = Router();
+
+router.get('/', (req, res) => {
+	res.render('index')
+})
+
---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express');
+const routes = require('./routes/')
+
+const app = express();
+
+const PORT = process.env.PORT || 3000
+app.set('port', PORT)
+
+
+//view engine
+// app.set('view engine', 'pug');
+
+
+//middlewares
+app.use(express.static('public'));
+
+//routes
+// app.use(routes);
+
+
+app.listen(PORT, () => console.log(`Express server listening on port: ${PORT}`))
