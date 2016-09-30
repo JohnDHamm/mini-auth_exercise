a49ac926d376b52d7d0c6a680d7ed17bb46b1bb1
John Hamm <johndhamm1@gmail.com>
2016-09-30 14:13:25 -0500

    remote mLab db connected

--- database.js
+++ database.js
@@ -2,8 +2,9 @@
 
 const mongoose = require('mongoose')
 // const MONGODB_URL = 'mongodb://localhost:27017/pizzabypug'
-const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/miniauth' //uses local
+// const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/miniauth' //uses local
 // const MONGODB_URL = 'mongodb://<username>:<password>@ds033106.mlab.com:33106/pizzabypug' //enter in Heroku
+const MONGODB_URL = 'mongodb://johnauth:16mini@ds047666.mlab.com:47666/miniauth'
 
 mongoose.Promise = Promise //tells mongoose to use global Promise vs. its' internal promise
 
